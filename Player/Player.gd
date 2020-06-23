142e4ce4a0d899a6b36d1132f61312884a26de37
AKPatrashkov18 <59877276+AKPatrashkov18@users.noreply.github.com>
2020-06-23 16:49:35 +0300

    Fixing bug

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,6 +15,7 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 export var ACCELERATION = 500
 export var MAX_SPEED = 80
@@ -84,3 +85,5 @@ func attack_animation_finished():
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= 1
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
