15bfe16d35d970622b43e0ec188d27f91b57e3fb
Georgi Kalchev <GSKalchev18@codingburgas.bg>
2020-06-28 19:32:45 +0300

    Add Pause Menu

---
+++ GameMenu/Pause.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		visible = true
+		get_tree().paused = true
+
+func _on_ContinueGameButton_pressed():
+	hide()
+	get_tree().paused = false
+
+
+func _on_QuitGameButton_pressed():
+	get_tree().paused = false
+	get_tree().quit()
